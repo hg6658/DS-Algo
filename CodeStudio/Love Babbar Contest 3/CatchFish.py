a0a06901b33ab6f07e1b7a0db294b5975341fc03
hg6658 <hg6658@bennett.edu.in>
2022-01-09 16:09:29 +0530

    updated file catch fish

--- CodeStudio/Love Babbar Contest 3/CatchFish.py
+++ CodeStudio/Love Babbar Contest 3/CatchFish.py
@@ -3,19 +3,22 @@ import sys
 def minimumNet(n: int, k: int, fish: List[int]) -> int:
     # Write your code here.
     l=0;
-    r=-1;
+    r=0;
     mini = 10**5;
+    cnt=0;
     for i in range(n):
         l=i;
         r=max(r,i);
         if(i-1>=0 and fish[i-1]>0):
             cnt-=1;
         while(r<n and cnt<k):
-            r+=1;
-            if(fish[i]=='1'):
+            if(fish[r]==1):
                 cnt+=1;
+            r+=1;     
         if(cnt==k):
-            mini = min(mini,r-l+1);
+            mini = min(mini,r-l);
+    if(mini==10**5):
+        return -1;
     return mini        
         
     
